c81d1a0ea1c5b1b9c9ee43ddf163a4c40d0b2eb0
Rutherford05 <nguingandjifils@gmail.com>
2019-07-19 09:10:54 +0000

    changer

--- app/src/main/java/com/example/myapp/Connexion.java
+++ app/src/main/java/com/example/myapp/Connexion.java
@@ -7,11 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Connexion extends AppCompatActivity {
+    private Button connect;
+    private TextInputEditText email;
+    private TextInputEditText password;
+    String mail,mot_de_passe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        email=(TextInputEditText) findViewById(R.id.valid_email);
+        password=(TextInputEditText) findViewById(R.id.valid_password);
+        connect=(Button) findViewById(R.id.button2);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
     public void creer(View v){
         Intent gerline= new Intent(Connexion.this,MainActivity.class);
