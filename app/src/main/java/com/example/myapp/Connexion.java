3d6c4067974f7688a903fe2cac0abed80ce4c508
Rutherford05 <nguingandjifils@gmail.com>
2019-07-19 17:54:03 +0000

    inscip

--- app/src/main/java/com/example/myapp/Connexion.java
+++ app/src/main/java/com/example/myapp/Connexion.java
@@ -1,30 +1,63 @@
 package com.example.myapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Connexion extends AppCompatActivity {
     private Button connect;
     private TextInputEditText email;
     private TextInputEditText password;
+    private ProgressBar progressBar;
     String mail,mot_de_passe;
+    private FirebaseAuth firebaseAuth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        firebaseAuth=FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()!=null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),Accueil.class));
+        }
         email=(TextInputEditText) findViewById(R.id.valid_email);
         password=(TextInputEditText) findViewById(R.id.valid_password);
         connect=(Button) findViewById(R.id.button2);
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                progressBar.setVisibility(View.VISIBLE);
+                mail=email.getText().toString();
+                mot_de_passe=password.getText().toString();
+                if(TextUtils.isEmpty(mail)){
+                    Toast.makeText(Connexion.this, "Veuillez entrer une adresse", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                 if(!email.getText().toString().trim().matches(emailPattern)){
+                    Toast.makeText(getApplicationContext(),"l'adresse saisie n'est pas valide",Toast.LENGTH_LONG).show();
+                }
+                 if(TextUtils.isEmpty(mot_de_passe)){
+                    Toast.makeText(Connexion.this,"Veuillez Entrer un mot de passe",Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         });
     }
--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,25 +2,98 @@ package com.example.myapp;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-   private Button btn2;
+    private TextInputEditText mNom;
+    private TextInputEditText mPrenom;
+    private TextInputEditText mEmail;
+    private TextInputEditText mMp;
+    private TextInputEditText mConfirm;
+    String mN,mP,mE,mC,mM;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private FirebaseAuth auth;
+     private Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth=FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() !=null){
+
+        }
+        mNom=(TextInputEditText) findViewById(R.id.Nom);
+        mPrenom=(TextInputEditText) findViewById(R.id.Prenom);
+        mEmail=(TextInputEditText) findViewById(R.id.email);
+        mMp=(TextInputEditText) findViewById(R.id.Password);
+        mConfirm=(TextInputEditText) findViewById(R.id.Confirmer);
         btn2=(Button) findViewById(R.id.button);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent naomie= new Intent(MainActivity.this,Connexion.class);
-                startActivity(naomie);
+                mN=mNom.getText().toString();
+                mP=mPrenom.getText().toString();
+                mE=mEmail.getText().toString();
+                mM=mMp.getText().toString();
+                mC=mConfirm.getText().toString();
+                if(!mEmail.getText().toString().trim().matches(emailPattern)){
+                    Toast.makeText(MainActivity.this,"Entrer une adresse valide",Toast.LENGTH_LONG).show();
+                }
+                  if(TextUtils.isEmpty(mN)){
+                    Toast.makeText(getApplicationContext(),"Entrer votre nom",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                 if(TextUtils.isEmpty(mP)){
+                     Toast.makeText(getApplicationContext(),"Entrer votre prenom",Toast.LENGTH_LONG).show();
+                     return;
+                }
+                  if (TextUtils.isEmpty(mE)){
+                     Toast.makeText(getApplicationContext(),"Entrer une adresse email",Toast.LENGTH_LONG).show();
+                     return;
+                }
+                  if(TextUtils.isEmpty(mM)){
+                     Toast.makeText(getApplicationContext(),"Entrer un mot de passe",Toast.LENGTH_LONG).show();
+                }
+                  if(!mConfirm.getText().toString().equals(mMp.getText().toString())){
+                     Toast.makeText(MainActivity.this,"Les mots de passe ne se correspondent pas",Toast.LENGTH_LONG).show();
+                }
+                auth.createUserWithEmailAndPassword(mE,mM).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Successfully registered",Toast.LENGTH_LONG).show();
+                        }else{
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+
+
             }
         });
+
     }
+
 }
